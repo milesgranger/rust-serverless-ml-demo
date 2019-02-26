f52096eba5ae5152080ebd1cc95b7f7e920d9695
Miles Granger <miles59923@gmail.com>
2019-02-26 20:09:27 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use lambda_http::{lambda, IntoResponse, Request};
+use lambda_runtime::{error::HandlerError, Context};
+use serde_json::{json, Value};
+
+fn main() {
+    lambda!(handler)
+}
+
+fn handler(event: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
+    Ok(json!({
+    "message": "Go Serverless v1.0! Your function executed successfully!"
+    }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handler_handles() {
+        let request = Request::default();
+        let expected = json!({
+        "message": "Go Serverless v1.0! Your function executed successfully!"
+        })
+        .into_response();
+        let response = handler(request, Context::default())
+            .expect("expected Ok(_) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
+}
