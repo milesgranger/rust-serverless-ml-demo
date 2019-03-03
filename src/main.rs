12367a76d930d23b6e5c1be860edb4ac9b29dbd4
Miles Granger <miles59923@gmail.com>
2019-03-03 05:01:42 +0100

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-
-use lambda_http::{lambda, IntoResponse, Request, RequestExt, http::Method};
+use lambda_http::{http::Method, lambda, IntoResponse, Request, RequestExt};
 use lambda_runtime::{error::HandlerError, Context};
-
-
+use ndarray::{Array1, Array2};
+use pyrus_nn::{
+    activations::Activation,
+    layers::{Dense, Layer},
+    network::Sequential,
+};
 use serde_json::{json, Value};
-use ndarray::{Array2, Array1};
-
-use pyrus_nn::{network::Sequential, layers::{Layer, Dense}, activations::Activation};
-
 
 mod objects;
 use objects::Payload;
@@ -17,25 +16,21 @@ fn main() {
 }
 
 fn handler(request: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
-
     // POST we're training, GET we're predicting
     match request.method() {
         &Method::GET => get_model(&request),
         &Method::POST => train_or_predict(&request),
-        _ => Ok(json!({"message": "Only POST and GET supported."}))
+        _ => Ok(json!({"message": "Only POST and GET supported."})),
     }
 }
 
-
-
 // Here the request is expect to contain 'X' in body
 fn train_or_predict(request: &Request) -> Result<Value, HandlerError> {
-
-    match request.payload::<Payload>()
-        .map_err(|_e| HandlerError::from("Failed to parse payload"))? {
-
+    match request
+        .payload::<Payload>()
+        .map_err(|_e| HandlerError::from("Failed to parse payload"))?
+    {
         Some(payload) => {
-
             // Will always have x and model
             let x_array = array_from_nested_vec(payload.x);
             let mut model = payload.model;
@@ -46,9 +41,10 @@ fn train_or_predict(request: &Request) -> Result<Value, HandlerError> {
                     let y_array = array_from_nested_vec(target);
                     model.fit(x_array.view(), y_array.view());
                     Ok(json!({"message": "Training round completed!", "model": model}))
-                },
+                }
                 None => {
-                    let out = model.predict(x_array.view())
+                    let out = model
+                        .predict(x_array.view())
                         .outer_iter()
                         .map(|v| v.to_vec())
                         .collect::<Vec<Vec<f32>>>();
@@ -56,24 +52,23 @@ fn train_or_predict(request: &Request) -> Result<Value, HandlerError> {
                     Ok(json!({"message": "Created predictions!", "predictions": out}))
                 }
             }
-        },
+        }
 
-        None => Err(HandlerError::from("No payload supplied!"))
+        None => Err(HandlerError::from("No payload supplied!")),
     }
-
-
 }
 
 fn array_from_nested_vec(vec: Vec<Vec<f32>>) -> Array2<f32> {
     let shape = (vec.len(), vec[0].len());
-    Array1::from_iter(vec.into_iter().flat_map(|v| v)).into_shape(shape).unwrap()
+    Array1::from_iter(vec.into_iter().flat_map(|v| v))
+        .into_shape(shape)
+        .unwrap()
 }
 
 // Request is expected to contain 'X' array in query string params
 fn get_model(_request: &Request) -> Result<Value, HandlerError> {
     Ok(json!({"message": "New model, just for you!", "model": build_base_model()}))
 }
-
 
 fn build_base_model() -> Sequential {
     let mut nn = Sequential::new();
--- src/objects.rs
+++ src/objects.rs
@@ -1,10 +1,9 @@
-use serde_derive::{Deserialize};
 use pyrus_nn::network::Sequential;
-
+use serde_derive::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Payload {
     pub x: Vec<Vec<f32>>,
     pub y: Option<Vec<Vec<f32>>>,
-    pub model: Sequential
+    pub model: Sequential,
 }
